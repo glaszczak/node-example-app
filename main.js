50d4fd52528d70faaca96fa6ba6d024521df0322
Grzegorz Laszczak <grzegorz.laszczak@gmail.com>
2021-01-20 13:40:19 +0100

    test message

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ http
   .createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    response.end('Welcome\n');
+    response.end('Hello\n');
   })
   .listen(port);
 
